5dab31b5e3a2103d90edc0f1b00523555c30dced
Martins Chukwudi <chukwudimartins39@gmail.com>
2025-10-19 17:35:37 +0100

    added user registration endpoint

---
+++ README.md
@@ -0,0 +1,12 @@
+POST /api/token/
+{
+  "username": "testuser",
+  "password": "StrongPass123"
+}
+
+{
+  "username": "testuser",
+  "email": "test@example.com",
+  "password": "StrongPass123",
+  "password2": "StrongPass123"
+}
---
+++ users/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+# ✅ Optional: Get user profile (for logged-in users)
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email
+        })
